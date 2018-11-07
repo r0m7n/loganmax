81c7be47fd42140ab676d8df6e36c600b821a340
r0m7n <44775129+r0m7n@users.noreply.github.com>
2018-11-07 14:08:05 +0300

    Delete urls.py

--- для Кати/djangomax/Infodate/urls.py
+++
@@ -1,8 +0,0 @@
-﻿from django.conf.urls import url
-from .import views
-
-app_name = 'Infodate'
-urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^Infodate', views.primer, name='primer'),
-]
